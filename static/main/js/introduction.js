07303eaa8581de0ef1f8de024456305a8acd9e43
yujirohara1 <35021147+yujirohara1@users.noreply.github.com>
2022-09-04 12:39:30 +0900

    a
    a

--- static/main/js/introduction.js
+++ static/main/js/introduction.js
@@ -1,7 +1,7 @@
 
 
 var a = '';
-a = a + '  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">  ';
+a = a + '  <div id="carouselExampleIndicators" class="carousel carousel-dark slide" data-bs-ride="carousel">  ';
 a = a + '    <div class="carousel-indicators">  ';
 a = a + '      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>  ';
 a = a + '      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>  ';
